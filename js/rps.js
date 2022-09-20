6c5c4a6c3819fca348a407c549ce1bd87993e211
Yannic1992 <yannic1992@gmx.net>
2022-09-20 18:17:52 +0200

    Add basic js functionality for ui

--- js/rps.js
+++ js/rps.js
@@ -1,35 +1,66 @@
 let pCount = 0;
 let cCount = 0;
+let playerSelection;
+let roundNumber = 1;
+const buttons = document.querySelectorAll("button");
 
 function getComputerChoise(){
     return Math.floor(Math.random() * 3 + 1);
 }
 
 function playRound(playerSelection, getComputerChoise){
-    if(playerSelection == "rock" && getComputerChoise == 2){
+    const winner = document.querySelector(".winner");
+    const roundWinner = document.querySelector(".round-winner");
+    if(playerSelection == "button-rock" && getComputerChoise == 2){
         cCount++;
-        console.log("You lose this round! Paper beats Rock.");
-    }else if(playerSelection == "rock" && getComputerChoise == 3){
+        roundWinner.textContent = "You lose this round! Paper beats Rock.";
+    }else if(playerSelection == "button-rock" && getComputerChoise == 3){
         pCount++;
-        console.log("You win this round! Rock beats Scissors.");
-    }else if(playerSelection == "paper" && getComputerChoise == 1){
+        roundWinner.textContent = "You win this round! Rock beats Scissors.";
+    }else if(playerSelection == "button-paper" && getComputerChoise == 1){
         pCount++;
-        console.log("You win this round! Paper beats Rock.");
-    }else if(playerSelection == "paper" && getComputerChoise == 3){
+        roundWinner.textContent = "You win this round! Paper beats Rock.";
+    }else if(playerSelection == "button-paper" && getComputerChoise == 3){
         cCount++;
-        console.log("You lose this round! Scissors beats Paper.");
-    }else if(playerSelection == "scissors" && getComputerChoise == 1){
+        roundWinner.textContent = "You lose this round! Scissors beats Paper.";
+    }else if(playerSelection == "button-scissors" && getComputerChoise == 1){
         cCount++;
-        console.log("You lose this round! Rock beats Scissors.");
-    }else if(playerSelection == "scissors" && getComputerChoise == 2){
+        roundWinner.textContent = "You lose this round! Rock beats Scissors.";
+    }else if(playerSelection == "button-scissors" && getComputerChoise == 2){
         pCount++;
-        console.log("You win this round! Scissors beats Paper.");
+        roundWinner.textContent = "You win this round! Scissors beats Paper.";
     }else{
-        console.log("This round is a tie.");
+        roundWinner.textContent = "This round is a tie.";
     }
-    
-    
+    let playerCount = document.querySelector(".player-count");
+    let compCount = document.querySelector(".comp-count");
+    playerCount.textContent = `Player: ${pCount}`;
+    compCount.textContent = `Computer: ${cCount}`;
+    if(pCount == 5 || cCount == 5){
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
+        if(pCount > cCount){
+            winner.textContent = "You win the game! Please reload the page.";
+        }else if(pCount < cCount){
+            winner.textContent = "You lose the game! Please reload the page."
+        }else{
+            winner.textContent = "The game is a tie.";
+        }
+    }
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerSelection = button.className;
+        playRound(playerSelection, getComputerChoise());
+        roundNumber++;
+        const roundCount = document.querySelector(".round-count");
+        roundCount.textContent = `Round: ${roundNumber}`;
+        
+        
+    });
+});
 
 /*function game(){
     let i = 1;
